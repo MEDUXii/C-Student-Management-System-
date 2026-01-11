bee3b33e8afffe7c161da3c08384ce2a7e87a257
Ahmed Mahmoud <115795985+MEDUXii@users.noreply.github.com>
2026-01-11 15:54:44 +0200

    Refactor student management system code

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-
 
 #define MAX_GRADE 400
 #define MIN_GRADE 0
@@ -10,84 +8,215 @@
 #define MIN_AGE 17
 #define MAX_AGE 25
 
-struct Student {
-    int Student_ID;
-    char Full_Name[50];
-    int Age;
-    float Total_Marks, Average;
-    char Grade;
+struct Student{
+    int student_id, age;
+    char fullname[50];
+    float total_marks, average;
+    char grade;
 };
 
-float calculateAverage(float Tmark){
-    return Tmark/4;
+float calculate_avg(float total_marks){
+    float res = total_marks / courses_num;
+    return res;
 }
 
-float calculateClassAverage(struct Student students[], int n){
-    float sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += students[i].Average;
-    }
-    return (sum / n);
-}
-
-char assignGrade(float avg){
-
-    if (avg >= 90)
-    {
-        return 'A';
-    }
-
-    else if (avg >= 80  && avg < 90) 
-    {
-        return 'B';
-    }
+char assign_grade(float average){
+    char grade;
     
-    else if (avg >= 70 && avg < 80)
+
+    if (average >= 90)
     {
-        return 'D';
+        grade = 'A';
+    }
+    else if (average >= 80 && average < 90)
+    {
+        grade = 'B';
+    }
+    else if (average >= 70 && average < 80)
+    {
+        grade = 'C';
+    }
+    else if (average >= 60 && average < 70)
+    {
+        grade = 'D';
+    }
+    else
+    {
+        grade = 'F';
     }
 
-     else if (avg >= 60 && avg < 70)
-    {
-        return 'C';
-    }
-
-    else if (avg < 60)
-    {
-        return 'F';
-    }
-    
+    return grade;
 }
 
-int searchStudent(struct Student students[], int n){
-    int SID;
-    printf("Enter Student ID : ");
-    scanf("%d", &SID);
-
-    for (int i = 0; i < n; i++)
-    {
-        if (SID == students[i].Student_ID)
-        {
-            return i;
+int isDuplicateID(int id, struct Student students[], int count) {
+    for (int i = 0; i < count; i++) {
+        if (students[i].student_id == id) {
+            return 1;  // duplicate found
         }
-        
     }
-    
-    return -1;
-    
+    return 0;  // no duplicate
 }
 
-void sortStudents(struct Student students[], int n){
+void getInput(struct Student students[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        // Here will be the inputs needed from user
+        printf("\nEnter The Name for student %d : ", i + 1);
+        // scanf(" %c" , &students[i].fullname);
+        getchar();
+        fgets(students[i].fullname, sizeof(students[i].fullname), stdin); 
+        students[i].fullname[strcspn(students[i].fullname, "\n")] = '\0'; // REMOVE newline
 
-   for(int i = 0; i < n; i++){
-                if(students[i].Total_Marks < students[i+1].Total_Marks){
-                struct Student temp;
-                temp = students[i];
-                students[i] = students[i+1];
-                students[i+1] = temp;
+        duplicated_id:
+        printf("Enter The student id for student \" %s \" : ", students[i].fullname);
+        scanf("%d", &students[i].student_id);
+
+        if (isDuplicateID(students[i].student_id, students, i)) {
+            printf("Duplicated Student ID!\n");
+            goto duplicated_id;
+        }
+
+
+        do
+        {
+            printf("Enter The Age for student \" %s \" : ", students[i].fullname);
+            scanf("%d", &students[i].age);
+            if(students[i].age < MIN_AGE || students[i].age > MAX_AGE){
+                printf("Invalid Range , 17 : 25\n");
             }
+        } while (students[i].age < MIN_AGE || students[i].age > MAX_AGE);
+
+        do
+        {
+            printf("Enter The student total Marks for student \" %s \" : ", students[i].fullname);
+            scanf("%f", &students[i].total_marks);
+            if(students[i].total_marks > MAX_GRADE || students[i].total_marks < MIN_GRADE){
+                printf("Invalid Range , 0 : 400\n");
+            }
+        } while (students[i].total_marks > MAX_GRADE || students[i].total_marks < MIN_GRADE);
+
+        // Here will be the calculated values (derrived attributes)
+        students[i].average = calculate_avg(students[i].total_marks);
+
+        students[i].grade = assign_grade(students[i].average);
     }
+}
+
+char* performanceCategory(char grade){
+    switch (grade)
+    {
+    case 'A':
+        return "Excellent";
+        break;
+    case 'B':
+        return "Good";
+        break; 
+    case 'C':
+        return "Satisfactory";
+        break;
+    case 'D':
+        return "Needs Improvement";
+        break;  
+    case 'F':
+        return "Needs Improvement";
+        break;                
+    default:
+        return "Unknown";
+        break;
+    }
+
+}
+
+void printStudents(struct Student students[], int n) {
+  
+
+    // Print table header
+    printf("+----+-----+----------------------+-------------+------------+-------+------------------+\n");
+    printf("| ID | Age | Name                 | Total Marks | Percentage | Grade | Performance      |\n");
+    printf("+----+-----+----------------------+-------------+------------+-------+------------------+\n");
+
+    // Print each student
+    for (int i = 0; i < n; i++) {
+        printf("| %-2d | %-3d | %-20s | %-11.2f | %-10.2f | %-5c | %-16s |\n",
+               students[i].student_id,
+               students[i].age,
+               students[i].fullname,
+               students[i].total_marks,
+               (students[i].total_marks / MAX_GRADE) * 100,
+               students[i].grade,
+               performanceCategory(students[i].grade));
+    }
+
+    printf("+----+-----+----------------------+-------------+------------+-------+------------------+\n");
+}
+
+int searchStudent(int studentId , struct Student students[],int n){
+    
+    for(int i = 0; i < n ; i++) {
+        if(studentId == students[i].student_id){
+          return i;
+        }  
+    }
+    return -1;
+}
+
+void sortStudents(struct Student students[] , int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = i ; j < n; j++){
+            if(students[i].total_marks < students[j].total_marks){
+            struct Student temp;
+            temp = students[i];
+            students[i] = students[j];
+            students[j] = temp;
+            }
+        }
+    }
+}
+
+double calculateClassAverage(struct Student students[] , int n){
+    double sum = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            sum += students[i].average;
+        }
+
+        double average = sum / n;
+        return average;
+}
+
+void updateStudent(struct Student *students,int n){
+
+    int id;
+    printf("Enter Student ID you want to update it : ");
+    scanf("%d",&id);
+
+    int index = searchStudent(id , students , n);
+
+    if(index == -1){
+        printf("The Student is not found");
+        return;
+    }
+    // This line creates a pointer refering to the needed student 
+    struct Student *st = students + index;
+
+
+    do {
+        printf("Enter new age for \"%s\": ", st->fullname);
+        scanf("%d", &st->age);
+    }while (st->age < MIN_AGE || st->age > MAX_AGE);
+    
+     do
+    {
+        printf("Enter new total marks for \"%s\": ", st->fullname);
+        scanf("%f", &st->total_marks);
+    } while (st->total_marks < MIN_GRADE || st->total_marks > MAX_GRADE);
+
+     st->average = calculate_avg(st->total_marks);
+     st->grade = assign_grade(st->total_marks);
+
+    printf("Student data updated successfully!\n");
+
+
 }
 
 void printMenu() {
@@ -100,213 +229,160 @@ void printMenu() {
     printf("4. SORT STUDENTS BY MARKS\n");
     printf("5. CALCULATE CLASS STATISTICS\n");
     printf("6. SAVE DATA TO FILE\n");
-    printf("7. UPDATE STUDENT RECORD\n");
-    printf("8. EXIT\n");
+    printf("7. LOAD DATA FROM FILE\n");
+    printf("8. UPDATE STUDENT RECORD\n");
+    printf("9. EXIT\n");
     printf("==========================================\n");
-    printf("Enter your choice (1-8): ");
+    printf("Enter your choice (1-9): ");
 }
 
-void print_students(struct Student students[], int n)
-{
-    printf("\n---------------------------------------------------------------\n");
-    printf("| ID   | Name                     | Age | Total Marks | Avgerage  | Grade |\n");
-    printf("---------------------------------------------------------------\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("| %-4d | %-24s | %-3d | %-5.1f | %-4.1f | %-1c |\n",
-               students[i].Student_ID,
-               students[i].Full_Name,
-               students[i].Age,
-               students[i].Total_Marks,
-               students[i].Average,
-               students[i].Grade);
-    }
-
-    printf("---------------------------------------------------------------\n");
-}
-
-int isValidName(char *name)
-{
-    for (int i = 0; name[i] != '\0'; i++)
-    {
-        if (!isalpha(name[i]) && name[i] != ' ')
-            return 0; // invalid
-    }
-    return 1; // valid
-}
-
-int isUniqueID(int id, int index, struct Student students[])
-{
-    for (int i = 0; i < index; i++)
-    {
-        if (students[i].Student_ID == id)
-            return 0; // not unique
-    }
-    return 1; // unique
-}
-
-void input_Student(struct Student students[], int n){
-
-    for (int i = 0; i < n; i++)
-    {
-        do{
-            printf("\nEnter The Name for student %d : ", i + 1);
-            getchar();
-            fgets(students[i].Full_Name, sizeof(students[i].Full_Name), stdin);
-            students[i].Full_Name[strcspn(students[i].Full_Name, "\n")] = '\0';
-
-        } while (!isValidName(students[i].Full_Name));
-
-        do{
-            printf("Enter The student id for student \"%s\" : ", students[i].Full_Name);
-            scanf("%d", &students[i].Student_ID);
-
-        } while (!isUniqueID(students[i].Student_ID, i, students));
-
-        do
-        {
-            printf("Enter The Age for student \" %s \" : ", students[i].Full_Name);
-            scanf("%d", &students[i].Age);
-        } while (students[i].Age < MIN_AGE || students[i].Age > MAX_AGE);
-
-        do
-        {
-            printf("Enter The student total Marks for student \" %s \" : ", students[i].Full_Name);
-            scanf("%f", &students[i].Total_Marks);
-            // if -> 
-        } while (students[i].Total_Marks > MAX_GRADE || students[i].Total_Marks < MIN_GRADE);
-
-        // Here will be the calculated values (derrived attributes)
-        students[i].Average = calculateAverage(students[i].Total_Marks);
-
-        students[i].Grade = assignGrade(students[i].Average);
-        
-    }
-    
-
-}
-
-void saveToFile(struct Student students[], int n){
-
+void saveToFile(struct Student students[] , int n){
     FILE *file_output;
-    file_output = fopen("Students.txt","w");
+
+    file_output = fopen("students.txt","w");
 
     for(int i = 0; i < n; i++){
         fprintf(file_output,"%d,%d,%s,%.2f,%.2f,%c\n",
-            students[i].Student_ID ,
-            students[i].Age, 
-            students[i].Full_Name , 
-            students[i].Total_Marks, 
-            students[i].Average,
-            students[i].Grade
+            students[i].student_id ,
+            students[i].age, 
+            students[i].fullname , 
+            students[i].total_marks, 
+            students[i].average,
+            students[i].grade
             );
     }
 
     fclose(file_output);
 
     printf("Data Saved Successfully!");
-
 }
 
-void updateStudent(struct Student *students, int n){
-    int id;
-    printf("Enter Student ID you want to update it : ");
-    scanf("%d",&id);
+void readFromFile(){
+    FILE *file_input;
 
-    int index = searchStudent(students , n);
+    file_input = fopen("students.txt","r");
 
-    if(index == -1){
-        printf("The Student is not found");
+    if(file_input == NULL){
+        printf("\nFile doesn\'t Exist!\n");
         return;
     }
-    // This line creates a pointer refering to the needed student 
-    struct Student *st = students + index;
 
-
-    do {
-        printf("Enter new age for \"%s\": ", st->Full_Name);
-        scanf("%d", &st->Age);
-    }while (st->Age < MIN_AGE || st->Age > MAX_AGE);
-    
-     do
-    {
-        printf("Enter new total marks for \"%s\": ", st->Full_Name);
-        scanf("%f", &st->Total_Marks);
-    } while (st->Total_Marks < MIN_GRADE || st->Total_Marks > MAX_GRADE);
-
-     st->Average = calculateAverage(st->Total_Marks);
-     st->Grade = assignGrade(st->Total_Marks);
-
-    printf("Student data updated successfully!\n");
-}
-
-int main() 
-{
     struct Student students[100];
-    int n;
+    int lines = 0;
+    char line[200];
+
+    while (fgets(line, sizeof(line), file_input)) {
+        struct Student student;
+        // remove newline if present
+        line[strcspn(line, "\n")] = 0;
+
+        if (sscanf(line, "%d,%d,%49[^,],%f,%f,%c",
+                &student.student_id,
+                &student.age,
+                student.fullname,
+                &student.total_marks,
+                &student.average,
+                &student.grade) == 6) {
+            students[lines++] = student;
+            if (lines >= 100) break;  // avoid overflow
+        }
+    }
+    fclose(file_input);
+
+    printStudents(students , lines);
+
+    
+    
+ }
+
+
+int main() {
+    struct Student students[100];
+    int n = 0; // number of students currently in the system
     int choice;
-    while (1)
-    {
+
+    while (1) {
         printMenu();
-        scanf("%d", &choice);        
-        switch (choice)
-        {
+        scanf("%d", &choice);
+
+        switch (choice) {
             case 1:
                 printf("Enter number of students to add: ");
                 scanf("%d", &n);
-                input_Student(students, n);
+                getInput(students, n);
                 break;
+
             case 2:
                 if (n == 0) {
                     printf("No students available to display.\n");
                 } else {
-                    print_students(students, n);
+                    printStudents(students, n);
                 }
                 break;
-            case 3:
-                int index = searchStudent(students, n);
-                if (index == -1)
-                {
-                    printf("Not Found\n");
-                }
-                else{
-                    printf("Student Found\n");
-                    print_students(&students[index], 1);
+
+            case 3: {
+                int id;
+                printf("Enter student ID to search: ");
+                scanf("%d", &id);
+                int index = searchStudent(id, students, n);
+                if (index == -1) {
+                    printf("Student not found.\n");
+                } else {
+                    printf("Student found:\n");
+                    printStudents(&students[index], 1);
                 }
                 break;
+            }
+
             case 4:
-                sortStudents(students, n);
-                printf("Students sorted by total marks (descending).\n");
+                if (n <= 1) {
+                    printf("Not enough students to sort.\n");
+                } else {
+                    sortStudents(students, n);
+                    printf("Students sorted by total marks (descending).\n");
+                }
                 break;
+
             case 5:
-                if(n == 0){
+                if (n == 0) {
                     printf("No students to calculate statistics.\n");
                 } else {
-                    printf("The Class Average is : %.2f\n",calculateClassAverage(students, n));
+                    double classAvg = calculateClassAverage(students, n);
+                    printf("Class Average: %.2f\n", classAvg);
                 }
                 break;
+
             case 6:
-                 if (n == 0) {
+                if (n == 0) {
                     printf("No students to save.\n");
                 } else {
                     saveToFile(students, n);
                 }
                 break;
+
             case 7:
+                readFromFile();
+                break;
+
+            case 8:
                 if (n == 0) {
                     printf("No students to update.\n");
                 } else {
                     updateStudent(students, n);
                 }
                 break;
-            case 8:
+
+            case 9:
+                printf("Exiting program. Goodbye!\n");
                 exit(0);
+
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Invalid choice! Please enter 1-9.\n");
+                break;
         }
+
+        printf("\n"); 
     }
-    
-    
-    
+
+    return 0;
 }
